0e7ead9bd3611eb440f8a1983c8844b3b9693aa7
paulstey <paulstey@gmail.com>
2016-12-12 08:32:09 -0500

    hello react app is running; start of 3.1

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send({ data: []} );
+  res.send({ data: [] });
 });
 
 export default router;

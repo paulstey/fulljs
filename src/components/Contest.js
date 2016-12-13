6e03a3328614a832c657fb2d69dfe833a1a52324
paulstey <paulstey@gmail.com>
2016-12-13 15:16:26 -0500

    first commit

---
+++ src/components/Contest.js
@@ -0,0 +1,16 @@
+import React, {Component, PropTypes} from 'react';
+
+class Contest extends Component {
+    render() {
+        return (
+            <div className="Contest">
+                {this.props.description}
+            </div>
+        );
+    }
+}
+
+Contest.propTypes = {
+    description: PropTypes.string.isRequired
+}
+export default Contest;
